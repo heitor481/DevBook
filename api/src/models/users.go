b2225105cf6b875b4acc6151149ba2c1862835e9
Heitor Ribeiro <heitor_481@hotmail.com>
2022-01-22 22:01:43 -0300

    Adding sql script to create database (using sqlServer)"

---
+++ api/src/models/users.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+//User represents a user using the social network
+type User struct {
+	ID         uint64    `json:"id,omitempty"`
+	Name       string    `json:"name,omitempty"`
+	Nick       string    `json:"nick,omitempty`
+	Email      string    `json:"email,omitempty`
+	Password   string    `json:"password,omitempty`
+	CreateDate time.Time `json:"createDate,omitempty`
+}
